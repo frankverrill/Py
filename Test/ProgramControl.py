92c2b4936984534849a3dd5d28206dcbdb8d51ff
Frank Verrill <frank.verrill@nuvoldirect.com>
2018-11-03 09:26:33 -0700

    Commit 1

---
+++ Test/ProgramControl.py
@@ -0,0 +1,23 @@
+# def clinic():
+#     print("You've just entered the clinic!")
+#     print("Do you take the door on the left or the right?")
+#     answer = input("Type left or right and hit 'Enter'.").lower()
+#     if answer == "left" or answer == "l":
+#         print("This is the Verbal Abuse Room, you heap of parrot droppings!")
+#     elif answer == "right" or answer == "r":
+#         print("Of course this is the Argument Room, I've told you that already!")
+#     else:
+#         print("You didn't pick left or right! Try again.")
+#         clinic()
+
+
+# clinic()
+bool_one = False and False
+
+bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
+
+bool_three = 19 % 4 != 300 / 10 / 10 and False
+
+bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
+
+bool_five = True and True
